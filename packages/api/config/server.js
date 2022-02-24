e242d2c89c5787d3be9467d9c96a85fe2a669045
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2022-02-24 08:34:28 -0300

    feat: update

--- packages/api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b29018b5271d5bfb0079ae1ed6832c17')
-    }
-  }
-})
--- packages/api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '9ef9ed1a-dbf1-4629-8e17-b5191c53dab4'
-}
